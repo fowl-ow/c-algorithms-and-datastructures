52248d1b6e6b3eb494c6590e1b2b5ded0f75e76d
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2024-11-22 19:47:47 +0100

    Implement queue using a linked list

--- datastructures/include/list_queue.h
+++ datastructures/include/list_queue.h
@@ -1,11 +1,13 @@
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef LIST_QUEUE_H
+#define LIST_QUEUE_H
 
 typedef struct Queue Queue;
 
 Queue* createQueue();
 void enqueue(Queue* q, int value);
 int dequeue(Queue* q);
+int peek(Queue* q);
 void destroyQueue(Queue* q);
+
 
 #endif
---
+++ datastructures/src/list_queue.c
@@ -0,0 +1,83 @@
+#include "../include/list_queue.h"
+#include <stdlib.h>  
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+struct Queue
+{
+    Node *front;
+    Node *rear;
+};
+
+Queue *createQueue()
+{
+    struct Queue *q = malloc(sizeof(struct Queue));
+    if (q == NULL) return NULL;
+    q->front = NULL;
+    q->rear = NULL;
+    return q;
+}
+
+void enqueue(Queue *q, int value)
+{
+    if (q == NULL) return;
+    Node *n = malloc(sizeof(Node));
+    n->data = value;
+    n->next = NULL;
+    if (q->front == NULL && q->rear == NULL)
+    {
+        q->front = n;
+        q->rear = n;
+    }
+    else
+    {
+        q->rear->next = n;
+        q->rear = n;
+    }
+}
+
+int dequeue(Queue *q)
+{
+    if (q == NULL || q->front == NULL) return -1;
+
+    Node *n = q->front;
+    if (n->next == NULL)
+    {
+        q->front = NULL;
+        q->rear = NULL;
+    }
+    else
+    {
+        q->front = n->next;
+    }
+    int data = n->data;
+    free(n);
+    return data;
+}
+
+int peek(Queue *q)
+{
+    if (q == NULL || q->front == NULL) return -1;
+    return q->front->data;
+}
+
+void destroyQueue(Queue *q)
+{
+    if (q == NULL) return;
+    if (q->front != NULL)
+    {
+        Node *n = q->front;
+        while (n->next != NULL)
+        {
+            Node *d = n;
+            n = n->next;
+            free(d);
+        }
+        free(n);
+    }
+    free(q);
+}
