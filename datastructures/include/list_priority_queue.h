4fe7a25f1004bea881ff4a77624de1aeea092bcb
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2024-11-22 20:07:45 +0100

    Add priority to enqueue() parameters

--- datastructures/include/list_priority_queue.h
+++ datastructures/include/list_priority_queue.h
@@ -4,7 +4,7 @@
 typedef struct MinQueue MinQueue;
 
 MinQueue *createQueue();
-void enqueue(MinQueue *q, int value);
+void enqueue(MinQueue *q, int value, int priority);
 int dequeue(MinQueue *q);
 int peek(MinQueue *q);
 void destroyQueue(MinQueue *q);
